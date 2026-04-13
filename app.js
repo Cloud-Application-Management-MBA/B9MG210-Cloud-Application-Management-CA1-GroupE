e7e60e5bbdf88f13f4d04877658f91a02432f977
pionalobo <pionalobo31@gmail.com>
2026-04-13 19:25:54 +0000

    Fix Azure binding

--- app.js
+++ app.js
@@ -195,6 +195,6 @@ app.post('/api/checkout', async (req, res) => {
 // ================= START SERVER =================
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
